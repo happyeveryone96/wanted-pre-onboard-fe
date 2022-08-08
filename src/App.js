b4e513a91771061e5f13ee7615f01de251b43227
happyeveryone96 <happyeveryone@naver.com>
2022-08-08 18:17:55 +0900

    Add: 라우터 설정 및 reset.scss 적용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginSignup from "./pages/LoginSignup/LoginSignup";
+import ToDo from "./pages/ToDo/ToDo";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginSignup />} />
+        <Route path="/todo" element={<ToDo />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
